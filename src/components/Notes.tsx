e5a3994c73e6cd760687081c1f35d7ec4d70b047
illdoodle <illdoodmail@gmail.com>
2024-04-14 18:01:33 +0300

    кнопка очистки, временное решение пометки "выполнено"

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -5,26 +5,30 @@ import NotesItem from './NotesItem';
 function Notes() { 
   const jsonNotes = localStorage.getItem('notes');
   const [notes, setNotes] = useState(jsonNotes ? JSON.parse(jsonNotes) : []);
-  let newNotes = notes;
   
-  function addNote(e:any, element:any){
-    e.preventDefault();
+  function addNote(element:any){
+    let newNotes = notes;
     for (let i = 0; i < newNotes.length; i++) {
       newNotes[i].id++;
     }
-    setNotes([{id: 0, text: element.value.trim() ? element.value : 'очень странная заметка...'}, ...notes]);
+    setNotes([{id: 1, text: element.value.trim() ? element.value : newNotes.length + 1, completed: false}, ...newNotes]);
     element.value = '';
   }
 
-  function removeNote(e:any, note:any) {
-    e.preventDefault();
-    newNotes.splice(note.id, 1);
-    for (let i = note.id; i < newNotes.length; i++) {
+  function removeNote(note:any) {
+    let newNotes = notes;
+    newNotes.splice(note.id - 1, 1);
+    for (let i = note.id - 1; i < newNotes.length; i++) {
       newNotes[i].id--;
     }
     setNotes([...newNotes]);
   }
 
+  function setComplete(note:any){
+    note.completed = !note.completed;   
+    setNotes([...notes]);
+  }
+  
   useEffect(() => {
     localStorage.setItem('notes', JSON.stringify(notes));
   }, [notes])
@@ -33,15 +37,18 @@ function Notes() {
     <div className={styles.root}>
         <div className={styles.header}>
           <h1>TO-DO LIST</h1>
+          <div className={styles.clear}>
+            <button onClick={() => setNotes([])}>CLEAR</button>
+          </div>
         </div>
         <div className={styles.input}>
-          <input type="text" placeholder='Enter...' className={styles.enter} id="notes-enter"
-            onKeyDown={(e) => {e.code == 'Enter' ? addNote(e, document.querySelector('#notes-enter')) : true}}
+          <input type="text" maxLength={50} placeholder='Enter...' className={styles.enter} id="notes-enter"
+            onKeyDown={(e) => {e.code == 'Enter' ? addNote(document.querySelector('#notes-enter')) : true}}
           />
-          <button className={styles.addBtn} onClick={(e) => {addNote(e, document.querySelector('#notes-enter'))}}>ADD</button>
+          <button className={styles.addBtn} onClick={() => {addNote(document.querySelector('#notes-enter'))}}>ADD</button>
         </div>
         <div className={styles.list}>
-          {notes.map((note:any) => <NotesItem delete={removeNote} key={note.id} note={note}/>)}
+          {notes.map((note:any) => <NotesItem delete={removeNote} check={setComplete} key={note.id} note={note}/>)}
         </div>
     </div>
   )
--- src/components/NotesItem.tsx
+++ src/components/NotesItem.tsx
@@ -3,14 +3,15 @@ import styles from '../styles/note.module.css';
 function NotesItem(props:any) {
   return (
     <div className={styles.note}>
-      <div className={styles.text}>
-        <span className={styles.noteId}>
-          <input type="checkbox" className={styles.checkbox}/>
-          {props.note.id}
-        </span>
-        {props.note.text}
+      <div className={styles.id}>
+        <span>{props.note.id}</span>
       </div>
-      <button className={styles.deleteBtn} onClick={(e) => {props.delete(e, props.note)}}>DELETE</button>
+      <div className={`${styles.text} ${props.note.completed ? styles.completed : ''}`} onClick={() => {props.check(props.note)}}>
+        <span id='text'>{props.note.text}</span>
+      </div>
+      <div className={styles.deleteBtn}>
+        <button onClick={() => {props.delete(props.note)}}>DELETE</button>
+      </div>
     </div>
   )
 }
